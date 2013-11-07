9a4a84ab3d2bd3d25d07f6334e7410e6674f34b3
cheng <spch2008@foxmail.com>
2013-11-07 17:01:19 +0800

    add stdio linux method

--- MinCRT/MinCRT.h
+++ MinCRT/MinCRT.h
@@ -1,6 +1,21 @@
 #ifndef _MINCRT_H
 #define _MINCRT_H
 
+#define NULL 0
+
+typedef int FILE;
+#define EOF (-1)
+
+#ifdef WIN32
+#define stdin  ( (FILE*)(GetStdHandle(STD_INPUT_HANDLE)))
+#define stdout ( (FILE*)(GetStdHandle(STD_OUTPUT_HANDLE)))
+#define stderr ( (FILE*)(GetStdHandle(STD_ERROR_HANDLE)))
+#else
+#define stdin  ((FILE*)0)
+#define stdout ((FILE*)1)
+#define stderr ((FILE*)2)
+#endif
+
 int crt_heap_init();
 int crt_io_init();
 #endif
--- MinCRT/stdio.cpp
+++ MinCRT/stdio.cpp
@@ -10,25 +10,26 @@ int crt_io_init()
 
 FILE * fopen(const char *filename, const char *mode)
 {
+	return NULL;
 }
 
 int fclose(FILE * stream)
 {
-	return CloseHandler((HANDLE)stream);
+	return CloseHandle((HANDLE)stream);
 }
 
 size_t fread(void *ptr, size_t size, size_t count, FILE *stream)
 {
 	int nreads = 0;
-	if( !ReadFile((HANDLE)stream, ptr, size * count, nreads, 0) )
+	if( !ReadFile((HANDLE)stream, ptr, size * count, (LPDWORD)&nreads, 0) )
 		return 0;
 	return nreads;
 }
 
-size_t fwrite(const void *ptr, size_t size, size_t count, FIFE *stream)
+size_t fwrite(const void *ptr, size_t size, size_t count, FILE *stream)
 {
 	int nwrites = 0;
-	if( !WriteFile( (HANDLE)stream, ptr, size * count, nwrites, 0) )
+	if( !WriteFile( (HANDLE)stream, ptr, size * count, (LPDWORD)&nwrites, 0) )
 		return 0;
 	return nwrites;
 }
@@ -38,4 +39,96 @@ int fseek(FILE *stream, long int offset, int origin)
 	return SetFilePointer((HANDLE)stream, offset, 0, origin);
 }
 
+#else
+
+static int open(const char *pathname, int flags, int mode)
+{
+	int fd = 0;
+	asm(" movl $5, %%eax \n\t"
+		" movl %1, %%ebx \n\t"
+		" movl %2, %%ecx \n\t"
+		" movl %3, %%edx \n\t"
+		" int $0x80      \n\t"
+		" movl %%eax, %0 \n\t":
+		" =m"(fd) : "m"(pathname), "m"(flags), "m"(mode));
+	return fd;
+}
+
+static int read(int fd, void *buffer, unsigned size)
+{
+	int ret = 0;
+	asm(" movl $3, %%eax \n\t"
+		" movl %1, %%ebx \n\t"
+		" movl %2, %%ecx \n\t"
+		" movl %3, %%edx \n\t"
+		" int $0x80      \n\t"
+		" movl %%eax, %0 \n\t":
+		"=m"(ret) : "m"(fd), "m"(buffer), "m"(size));
+	return ret;
+}
+
+static int write(int fd, const void *buffer, unsigned size)
+{
+	int ret = 0;
+	asm(" movl $4, %%eax \n\t"
+		" movl %1, %%ebx \n\t"
+		" movl %2, %%ecx \n\t"
+		" movl %3, %%edx \n\t"
+		" int $0x80      \n\t"
+		" movl %%eax, %0 \n\t":
+	    "=m"(ret):"m"(fd), "m"(buffer), "m"(size) );
+	return ret;
+}
+
+
+static int close(int fd)
+{
+	int ret = 0;
+	asm(" movl $6, %%eax \n\t"
+		" movl %1, %%ebx \n\t"
+		" int $0x80      \n\t"
+		"movl %%eax, %0  \n\t" :
+	    "=m"(ret) : "m"(fd));
+	return ret;
+}
+
+
+static int seek(int fd, int offset, int mod)
+{
+	int ret = 0;
+	ams(" movl $19, %%eax \n\t"
+		" movl %1,  %%ebx \n\t"
+		" movl %2,  %%ecx \n\t"
+		" movl %3,  %%edx \n\t"
+		" int $0x80       \n\t"
+		" movl %%eax, %0  \n\t" :
+	    "=m"(ret):"m"(fd),"m"(offset), "m"(mode));
+	return ret;
+}
+
+
+FILE * fopen(const char *filename, const char *mode)
+{
+	return NULL;
+}
+
+int fclose(FILE * stream)
+{
+	return close(stream);
+}
+
+size_t fread(void *ptr, size_t size, size_t count, FILE *stream)
+{
+	return read(stream, ptr, size * count);
+}
+
+size_t fwrite(const void *ptr, size_t size, size_t count, FIFE *stream)
+{
+	return write(stream, ptr, size * count);
+}
+
+int fseek(FILE *stream, long int offset, int origin)
+{
+	return seek(stream, offset, origin);
+}
 #endif
